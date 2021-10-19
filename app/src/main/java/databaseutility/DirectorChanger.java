99a0087b36d8941b869896eea588f599f651d73c
Ali <alie0302@uni.sydney.edu.au>
2021-10-20 10:38:26 +1100

    added some test specs for databaseutility

--- app/src/main/java/databaseutility/DirectorChanger.java
+++ app/src/main/java/databaseutility/DirectorChanger.java
@@ -3,8 +3,8 @@ package databaseutility;
 import MTBMS.Database;
 
 public class DirectorChanger {
-    public static void changeDirectors(Database d, String movieName, String newDirector){
-        String query = String.format("UPDATE Movie SET director = '%s' WHERE name = '%s';", newDirector, movieName);
+    public static void changeDirectors(Database d, String movieName, String newDirectors){
+        String query = String.format("UPDATE Movie SET director = '%s' WHERE name = '%s';", newDirectors, movieName);
         d.sql_update(query);
     }
 }
