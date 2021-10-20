c1c4e16dd3800641c1650abb1f71274d4e9131dc
Ali <alie0302@uni.sydney.edu.au>
2021-10-21 00:03:02 +1100

    -

--- app/src/main/java/MTBMS/BookingSystem.java
+++ app/src/main/java/MTBMS/BookingSystem.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
+import static databaseutility.UserAuthenticate.authenticate;
 
 
 
