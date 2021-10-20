fbabb716e36314eab2c688d104ca50fea296ba2e
Natalie Lu <jilu8927@uni.sydney.edu.au>
2021-10-20 11:48:53 +0800

    minior changes

--- app/src/main/java/databaseutility/MovieNameChanger.java
+++ app/src/main/java/databaseutility/MovieNameChanger.java
@@ -2,9 +2,8 @@ package databaseutility;
 
 import MTBMS.Database;
 
-public class MovieNameChanger {
+public class  MovieNameChanger {
     public static void changeMovieName(Database d, String previousName, String newName) {
-
         String query = String.format("UPDATE Movie SET name = '%s' WHERE name = '%s';", newName, previousName);
         d.sql_update(query);
     }
