9fe95626273c3a04641eaee341e9d0ad922aaea6
Ali <alie0302@uni.sydney.edu.au>
2021-10-20 23:43:40 +1100

    deleted staff.java and commmented out some code which isn't useful at the moment

--- app/src/main/java/databaseutility/MovieColumnsDisplay.java
+++ app/src/main/java/databaseutility/MovieColumnsDisplay.java
@@ -3,7 +3,7 @@ package databaseutility;
 import MTBMS.Database;
 
 public class MovieColumnsDisplay {
-    public static void displayMovieColumns(Database d){
+/*     public static void displayMovieColumns(Database d){
         String query = "SELECT * FROM information_schema.columns WHERE table_schema = 'moviebooking_db' AND table_name = 'movie';";
         String result = d.sql_getString(query, "column_name");
         int index = 1;
@@ -11,6 +11,6 @@ public class MovieColumnsDisplay {
             System.out.println(String.format("%d. %s", index, result));
             index++;
         }
-    }
+    } */
     
 }
