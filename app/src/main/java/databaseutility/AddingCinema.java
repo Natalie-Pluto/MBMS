04a22967b0baec7e0b53d52ab8d7a749234ddda9
Ali <alie0302@uni.sydney.edu.au>
2021-10-22 14:46:34 +1100

    a added some databaseutility classes

---
+++ app/src/main/java/databaseutility/AddingCinema.java
@@ -0,0 +1,10 @@
+package databaseutility;
+
+import MTBMS.Database;
+
+public class AddingCinema {
+    public static boolean addCinema(Database db, String username) {
+        boolean updateStatus = db.sql_update(String.format("insert into moviebooking_db.Cinema values('%s') on conflict (cinema_name) do nothing;;",username));
+        return updateStatus;
+    }
+}
