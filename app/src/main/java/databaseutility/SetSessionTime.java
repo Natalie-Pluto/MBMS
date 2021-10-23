70f7fa69736b75a3ba0af4b878d885ad1adbf56a
Ali <alie0302@uni.sydney.edu.au>
2021-10-23 20:00:21 +1100

    fixed bug with session time setting method and added methods for setting ticket price for adults, kids and seniors

--- app/src/main/java/databaseutility/SetSessionTime.java
+++ app/src/main/java/databaseutility/SetSessionTime.java
@@ -9,7 +9,7 @@ public class SetSessionTime {
     }
 
     public static boolean setEndTime(Database db, String movieName, String startTime, String cinemaName, String screenType, String newEndTime) {
-        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET end_time = '%s' where cinema = '%s' and screen_type = '%s' and movie = '%s' and end_time = '%s';", newEndTime, cinemaName, screenType, movieName, startTime));
+        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET end_time = '%s' where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newEndTime, cinemaName, screenType, movieName, startTime));
         return updateStatus;
     }
 }
---
+++ app/src/main/java/databaseutility/SetTicketPrice.java
@@ -0,0 +1,20 @@
+package databaseutility;
+
+import MTBMS.Database;
+
+public class SetTicketPrice {
+    public static boolean setKidsTicketPrice(Database db, String movieName, String startTime, String cinemaName, String screenType, double newPrice) {
+        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_kids = %f where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName, screenType, movieName, startTime));
+        return updateStatus;
+    }
+
+    public static boolean setAdultsTicketPrice(Database db, String movieName, String startTime, String cinemaName, String screenType, double newPrice) {
+        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_adults = %f where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName, screenType, movieName, startTime));
+        return updateStatus;
+    }
+
+    public static boolean setSeniorsTicketPrice(Database db, String movieName, String startTime, String cinemaName, String screenType, double newPrice) {
+        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_seniors = %f where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName, screenType, movieName, startTime));
+        return updateStatus;
+    }
+}
