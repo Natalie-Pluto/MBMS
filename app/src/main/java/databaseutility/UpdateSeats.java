d3b10fc4c586eca46e59b085185a30941b440129
Ali <alie0302@uni.sydney.edu.au>
2021-10-23 15:11:35 +1100

    removed a redundant class and updated the updateSeats method so that front, mid or back seats can be booked

--- app/src/main/java/databaseutility/UpdateSeats.java
+++ app/src/main/java/databaseutility/UpdateSeats.java
@@ -4,18 +4,21 @@ import MTBMS.Database;
 import utils.StringUtils;
 
 public class UpdateSeats {
-    public void updateSeats(Database db, String cinemaName, String movieName, String startTime, int numBooked){
+    public void updateSeats(Database db, String cinemaName, String movieName, String startTime, String screenType, int numBooked, String seatLocation){
         if (StringUtils.isNotEmpty(cinemaName) && StringUtils.isNotEmpty(movieName)
                 && StringUtils.isNotEmpty(startTime)){
-            db.sql_update(String.format("update cinema_session set number_of_front_seats_booked = %s where " +
-                    "cinema_name = '%s' and movie = '%s' and start_time = to_timestamp(%s);",
-                    numBooked,cinemaName,movieName,startTime));
-            db.sql_update(String.format("update cinema_session set number_of_mid_seats_booked = %s where " +
-                            "cinema_name = '%s' and movie = '%s' and start_time = to_timestamp(%s);",
-                    numBooked,cinemaName,movieName,startTime));
-            db.sql_update(String.format("update cinema_session set number_of_back_seats_booked = %s where " +
-                            "cinema_name = '%s' and movie = '%s' and start_time = to_timestamp(%s);",
-                    numBooked,cinemaName,movieName,startTime));
+                        if (seatLocation.equals("front"))
+                                db.sql_update(String.format("update cinema_session set number_of_front_seats_booked = %s where " +
+                                        "cinema_name = '%s' and movie = '%s' and start_time = to_timestamp(%s) and screen_type = '%s';",
+                                        numBooked,cinemaName,movieName,startTime,screenType));
+                        else if (seatLocation.equals("mid"))
+                                db.sql_update(String.format("update cinema_session set number_of_mid_seats_booked = %s where " +
+                                                "cinema_name = '%s' and movie = '%s' and start_time = to_timestamp(%s) and screen_type = '%s';",
+                                        numBooked,cinemaName,movieName,startTime,screenType));
+                        else if (seatLocation.equals("back")) 
+                                db.sql_update(String.format("update cinema_session set number_of_back_seats_booked = %s where " +
+                                                "cinema_name = '%s' and movie = '%s' and start_time = to_timestamp(%s) and screen_type = '%s';",
+                                        numBooked,cinemaName,movieName,startTime,screenType));
         }
     }
 }
