4c4faf8597d3972e242e10c4167595fa4aae520f
Ali <alie0302@uni.sydney.edu.au>
2021-10-23 15:46:32 +1100

    added method for redeeming gift card

--- app/src/main/java/databaseutility/RedeemingGiftCard.java
+++ app/src/main/java/databaseutility/RedeemingGiftCard.java
@@ -2,10 +2,10 @@ package databaseutility;
 
 import MTBMS.Database;
 
-public class RedeemedCheck{
-    public static boolean giftCardRedeemed(Database d, String giftCardNumber) {
+public class RedeemingGiftCard{
+    public static boolean redeemGiftCard(Database d, String giftCardNumber) {
         // returns false if either the gift card is not redeemed or the gift card is non-existent
-        String query = String.format("SELECT redeemed FROM moviebooking_db.Gift_Card where number = '%s';", giftCardNumber);
+        String query = String.format("UPDATE moviebooking_db.Gift_Card SET redeemed = true where number = '%s';", giftCardNumber);
         Boolean i = d.sql_getBoolean(query, "count");
         if (i ==  null) return false;
         return i;
