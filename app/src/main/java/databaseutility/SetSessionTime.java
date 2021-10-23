842f72b0c2df33519ee10de48ef6552f22c8d46a
Ali <alie0302@uni.sydney.edu.au>
2021-10-23 19:48:33 +1100

    added methods for changing the starting and ending times of a cinema session.

---
+++ app/src/main/java/databaseutility/SetSessionTime.java
@@ -0,0 +1,15 @@
+package databaseutility;
+
+import MTBMS.Database;
+
+public class SetSessionTime {
+    public static boolean setStartTime(Database db, String movieName, String startTime, String cinemaName, String screenType, String newStartTime) {
+        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET start_time = '%s' where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newStartTime, cinemaName, screenType, movieName, startTime));
+        return updateStatus;
+    }
+
+    public static boolean setEndTime(Database db, String movieName, String startTime, String cinemaName, String screenType, String newEndTime) {
+        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET end_time = '%s' where cinema = '%s' and screen_type = '%s' and movie = '%s' and end_time = '%s';", newEndTime, cinemaName, screenType, movieName, startTime));
+        return updateStatus;
+    }
+}
