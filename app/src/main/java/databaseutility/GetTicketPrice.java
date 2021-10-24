634e8653e4cd1deae2838d8b6118701928ef1612
Ali <alie0302@uni.sydney.edu.au>
2021-10-24 11:15:56 +1100

    split single method for getting ticket prices into 4 methods each for getting the ticket price of one type of session customer (i.e. kids/adults etc). Reason for this: seems simpler

--- app/src/main/java/databaseutility/GetTicketPrice.java
+++ app/src/main/java/databaseutility/GetTicketPrice.java
@@ -4,22 +4,51 @@ import MTBMS.Database;
 import utils.StringUtils;
 
 public class GetTicketPrice {
-    public static String getTicketPrice(Database db, String cinemaName, String movieName,String startTime, String type){
+    public static Double getTicketPriceKids(Database db, String cinemaName, String movieName,String startTime, String screenType){
         if (StringUtils.isEmpty(cinemaName)|| StringUtils.isEmpty(movieName)
-                ||StringUtils.isEmpty(startTime)||StringUtils.isEmpty(type)){
-            return "";
+                ||StringUtils.isEmpty(startTime)){
+            return null;
+        }else {
+            double kidPrice = db.sql_getDouble(String.format("select ticket_price_kids from cinema_session " +
+                    "where cinema = '%s' and movie = '%s' and start_time = to_timestamp(%s) " +
+                    "and screen_type = '%s';",cinemaName,movieName,startTime,screenType), "ticket_price_kids");
+            return kidPrice;
+        }
+    }
+
+    public static Double getTicketPriceAdults(Database db, String cinemaName, String movieName,String startTime, String screenType){
+        if (StringUtils.isEmpty(cinemaName)|| StringUtils.isEmpty(movieName)
+                ||StringUtils.isEmpty(startTime)||StringUtils.isEmpty(screenType)){
+            return null;
         }else {
             double adultPrice = db.sql_getDouble(String.format("select ticket_price_adults from cinema_session " +
                     "where cinema = '%s' and movie = '%s' and start_time = to_timestamp(%s) " +
-                    "and screen_type = '%s';",cinemaName,movieName,startTime,type), "ticket_price_adults");
-            double kidPrice = db.sql_getDouble(String.format("select ticket_price_kids from cinema_session " +
-                    "where cinema = '%s' and movie = '%s' and start_time = to_timestamp(%s) " +
-                    "and screen_type = '%s';",cinemaName,movieName,startTime,type), "ticket_price_kids");
-            double seniorPrice = db.sql_getDouble(String.format("select ticket_price_seniors from cinema_session " +
-                    "where cinema = '%s' and movie = '%s' and start_time = to_timestamp(%s) " +
-                    "and screen_type = '%s';",cinemaName,movieName,startTime,type), "ticket_price_seniors");
+                    "and screen_type = '%s';",cinemaName,movieName,startTime,screenType), "ticket_price_adults");
+            return adultPrice;
+        }
+    }
 
-            return adultPrice+"/"+kidPrice+"/"+seniorPrice;
+    public static Double getTicketPriceStudents(Database db, String cinemaName, String movieName,String startTime, String screenType){
+        if (StringUtils.isEmpty(cinemaName)|| StringUtils.isEmpty(movieName)
+                ||StringUtils.isEmpty(startTime)||StringUtils.isEmpty(screenType)){
+            return null;
+        }else {
+            double studentPrice = db.sql_getDouble(String.format("select ticket_price_students from cinema_session " +
+                    "where cinema = '%s' and movie = '%s' and start_time = to_timestamp(%s) " +
+                    "and screen_type = '%s';",cinemaName,movieName,startTime,screenType), "ticket_price_students");
+            return studentPrice;
+        }
+    }
+
+    public static Double getTicketPriceSeniors(Database db, String cinemaName, String movieName,String startTime, String screenType){
+        if (StringUtils.isEmpty(cinemaName)|| StringUtils.isEmpty(movieName)
+                ||StringUtils.isEmpty(startTime)||StringUtils.isEmpty(screenType)){
+            return null;
+        }else {
+            double seniorsPrice = db.sql_getDouble(String.format("select ticket_price_seniors from cinema_session " +
+                    "where cinema = '%s' and movie = '%s' and start_time = to_timestamp(%s) " +
+                    "and screen_type = '%s';",cinemaName,movieName,startTime,screenType), "ticket_price_seniors");
+            return seniorsPrice;
         }
     }
 }
