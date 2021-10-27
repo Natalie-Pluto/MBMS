fab5e3d2a972569c95b88d36be2070cea637866c
Ali <alie0302@uni.sydney.edu.au>
2021-10-27 12:01:43 +1100

    added methods in database utility for chanding the seat capacity. these three new methods replace the old single  method for changing the seat capacity of front, mid or back for any session

--- app/src/main/java/databaseutility/ChangeSeatCapacity.java
+++ app/src/main/java/databaseutility/ChangeSeatCapacity.java
@@ -2,7 +2,16 @@ package databaseutility;
 
 import MTBMS.Database;
 public class ChangeSeatCapacity {
-    public static void changeSeatCapacity(Database d, String cinemaName, String screenType, String movieName, String startTime, String newSeatCapacity, String seatLocation){
-            d.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET %s_seat_capacity = '%s' WHERE cinema = '%s' and screen_type = %s and movie = %s and start_time = %s;", seatLocation, newSeatCapacity, cinemaName, screenType, movieName, startTime));
+    public static void changeFrontSeatCapacity(Database d, String cinemaName, String movieName, String screenType, String startTime, int newSeatCapacity){
+            d.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET front_seat_capacity = '%s' WHERE cinema = '%s' and screen_type = %s and movie = %s and start_time = %s;", newSeatCapacity, cinemaName, screenType, movieName, startTime));
     }
+
+    public static void changeMidSeatCapacity(Database d, String cinemaName, String movieName, String screenType, String startTime, int newSeatCapacity){
+        d.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET mid_seat_capacity = '%s' WHERE cinema = '%s' and screen_type = %s and movie = %s and start_time = %s;", newSeatCapacity, cinemaName, screenType, movieName, startTime));
+    }
+
+    public static void changeBackSeatCapacity(Database d, String cinemaName, String movieName, String screenType, String startTime, int newSeatCapacity){
+        d.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET back_seat_capacity = '%s' WHERE cinema = '%s' and screen_type = %s and movie = %s and start_time = %s;", newSeatCapacity, cinemaName, screenType, movieName, startTime));
+    }
+
 }
