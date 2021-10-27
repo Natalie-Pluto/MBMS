1ada1fd918fefe60210696fb776d875e9305cdda
Ali <alie0302@uni.sydney.edu.au>
2021-10-27 21:40:05 +1100

    debugging

--- app/src/test/java/MTBMS/databaseUtilityTests.java
+++ app/src/test/java/MTBMS/databaseUtilityTests.java
@@ -83,10 +83,10 @@ import databaseutility.CheckIfSessionExists;
 import databaseutility.MovieInsertionBuilder;
 
 public class databaseUtilityTests {
-    //static Database dbInstance = new Database("jdbc:postgresql://ls-d4381878930280384f33af335289e24c73224a04.c0apyqxz8x8m.ap-southeast-2.rds.amazonaws.com:5432/postgres",
-    //"dbmasteruser","A>XV>D*7r-V{y_wL}}I{+U=8zEtj1*T<");
+    static Database dbInstance = new Database("jdbc:postgresql://ls-d4381878930280384f33af335289e24c73224a04.c0apyqxz8x8m.ap-southeast-2.rds.amazonaws.com:5432/postgres",
+    "dbmasteruser","A>XV>D*7r-V{y_wL}}I{+U=8zEtj1*T<");
 
-    static Database dbInstance =  new Database("jdbc:postgresql://localhost:5432/postgres", "postgres", "0000");
+    //static Database dbInstance =  new Database("jdbc:postgresql://localhost:5432/postgres", "postgres", "0000");
 
     @Test
     public void AddingUser_1() {
