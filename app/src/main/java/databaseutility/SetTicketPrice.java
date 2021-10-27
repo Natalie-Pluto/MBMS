f442471a35ef5ee5c8753d8b7e76a096da28d4dc
Ali <alie0302@uni.sydney.edu.au>
2021-10-28 00:05:55 +1100

    added tests, debugging

--- app/src/main/java/databaseutility/SetTicketPrice.java
+++ app/src/main/java/databaseutility/SetTicketPrice.java
@@ -4,22 +4,22 @@ import MTBMS.Database;
 
 public class SetTicketPrice {
     public static boolean setKidsTicketPrice(Database db, String movieName, String startTime, String cinemaName, String screenType, double newPrice) {
-        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_kids = %f where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName, screenType, movieName, startTime));
+        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_kids = %s where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName.replace("'","''"), screenType, movieName.replace("'","''"), startTime));
         return updateStatus;
     }
 
     public static boolean setAdultsTicketPrice(Database db, String movieName, String startTime, String cinemaName, String screenType, double newPrice) {
-        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_adults = %f where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName, screenType, movieName, startTime));
+        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_adults = %s where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName.replace("'","''"), screenType, movieName.replace("'","''"), startTime));
         return updateStatus;
     }
 
     public static boolean setSeniorsTicketPrice(Database db, String movieName, String startTime, String cinemaName, String screenType, double newPrice) {
-        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_seniors = %f where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName, screenType, movieName, startTime));
+        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_seniors = %s where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName.replace("'","''"), screenType, movieName.replace("'","''"), startTime));
         return updateStatus;
     }
 
     public static boolean setStudentsTicketPrice(Database db, String movieName, String startTime, String cinemaName, String screenType, double newPrice) {
-        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_students = %f where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName, screenType, movieName, startTime));
+        boolean updateStatus = db.sql_update(String.format("UPDATE moviebooking_db.Cinema_Session SET ticket_price_students = %s where cinema = '%s' and screen_type = '%s' and movie = '%s' and start_time = '%s';", newPrice, cinemaName.replace("'","''"), screenType, movieName.replace("'","''"), startTime));
         return updateStatus;
     }
 }
