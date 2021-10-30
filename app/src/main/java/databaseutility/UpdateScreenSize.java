9234bf03ff011eaeb301defb4fe1773b6fe97ff4
Ali <alie0302@uni.sydney.edu.au>
2021-10-30 13:26:47 +1100

    commented out functionaliy for updating screen type since it is part of the pk of a session and doesn't seem essential

--- app/src/main/java/databaseutility/UpdateScreenSize.java
+++ app/src/main/java/databaseutility/UpdateScreenSize.java
@@ -3,10 +3,11 @@ package databaseutility;
 import MTBMS.Database;
 import utils.StringUtils;
 
-public class UpdateScreenSize {
+/* public class UpdateScreenSize {
     public static void updateScreenSize(Database db, String cinemaName, String movieName, String startTime, String screenType){
             db.sql_update(String.format("update cinema_session set screen_type = %s where " +
                     "cinema_name = '%s' and movie = '%s' and start_time = to_timestamp(%s);",
                     screenType,cinemaName,movieName,startTime));
         }
 }
+ */
