c335bef3615ac0798040519e62debf09abefdbb0
Natalie Lu <jilu8927@uni.sydney.edu.au>
2021-10-31 13:15:09 +0800

    na

--- app/src/main/java/databaseutility/FilterNowshowingCinema.java
+++ app/src/main/java/databaseutility/FilterNowshowingCinema.java
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FilterNowshowingCinema {
-    public static List<String> filternowshowingCinema(Database db, String cinemaName){
-        if (cinemaName == null || cinemaName.length() == 0){
+    public static List<String> filternowshowingCinema(Database db, String cinemaName) {
+        if (cinemaName == null || cinemaName.length() == 0) {
             return new ArrayList<>();
-        }else {
-            return db.sql_getStrings("select distinct movie from moviebooking_db.cinema_session where cinema = '"+ cinemaName+"' and start_time <= current_date + 2;", "movie");
+        } else {
+            return db.sql_getStrings("select distinct movie from moviebooking_db.cinema_session where cinema = '" + cinemaName + "' and start_time >= current_date;", "movie");
         }
     }
+
 }
