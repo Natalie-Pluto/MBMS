174920ca75a3729899417f7b428e8e8274f21145
Albert Wang <xwan2252@uni.sydney.edu.au>
2021-11-01 21:16:58 +1100

    updated single quote on sql query

--- app/src/main/java/databaseutility/CastsChanger.java
+++ app/src/main/java/databaseutility/CastsChanger.java
@@ -4,7 +4,7 @@ import MTBMS.Database;
 
 public class CastsChanger {
     public static boolean changeCasts(Database d, String movieName, String newCasts){
-        String query = String.format("UPDATE moviebooking_db.Movie SET cast_ = '%s' WHERE name_ = '%s';", newCasts, movieName);
+        String query = String.format("UPDATE moviebooking_db.Movie SET cast_ = '%s' WHERE name_ = '%s';", newCasts.replace("'", "''"), movieName.replace("'", "''"));
         boolean success = d.sql_update(query);
         return success;
     }
--- app/src/main/java/databaseutility/SynopsisChanger.java
+++ app/src/main/java/databaseutility/SynopsisChanger.java
@@ -4,7 +4,7 @@ import MTBMS.Database;
 
 public class SynopsisChanger {
     public static boolean changeSynopsis(Database d, String movieName, String newSynopsis){
-        String query = String.format("UPDATE moviebooking_db.Movie SET synopsis = '%s' WHERE name_ = '%s';", newSynopsis, movieName);
+        String query = String.format("UPDATE moviebooking_db.Movie SET synopsis = '%s' WHERE name_ = '%s';", newSynopsis.replace("'", "''"), movieName.replace("'", "''"));
         boolean success = d.sql_update(query);
         return success;
     }
